fe4f028aa341647404c09f09f39b0464bb80e220
Yucel Ismail <ismailycel@gmail.com>
2022-05-08 22:20:25 +0300

    res api

---
+++ src/components/MovieCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const baseImageUrl = "https://image.tmdb.org/t/p/w1280";
+const defaultImage =
+    "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
+
+const MovieCard = () => {
+    return (
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+            <div className="col">
+                <div className="card">
+                    <img src="..." className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MovieCard;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const API_KEY = process.env.REACT_APP_TMDB_KEY;
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+// const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&include_adult=true`;
 
 const Main = () => {
+    const [movies, setMovies] = useState([]);
+    const getMovies = (API) => {
+        axios
+            .get(API)
+            // .then((res) => console.log(res))
+            .then((res) => setMovies(res.data.results))
+            .catch((err) => console.log(err))
+    };
+
+    useEffect(() => {
+        getMovies(FEATURED_API)
+    }, [])
+
+
     return (
-        <div>Main</div>
+        <>
+            {
+                movies.map(movie => (
+                    <MovieCard key={movie.id} {...movie} />
+                ))
+            }
+        </>
     )
 }
 
-export default Main
+export default Main;
